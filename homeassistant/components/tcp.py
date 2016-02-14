b8361bca450af13cbfdf0426c0efb6be0b18b551
Flyte <flyte@failcode.co.uk>
2016-02-14 00:03:56 +0000

    Add initial tcp component and Sensor

---
+++ homeassistant/components/tcp.py
@@ -0,0 +1,11 @@
+"""
+homeassistant.components.tcp
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+A generic TCP socket component.
+"""
+DOMAIN = "tcp"
+
+
+def setup(hass, config):
+    """ Nothing to do! """
+    return True
